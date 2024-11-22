609a2360d34e1b52e664ff9f2c501dc0694077d0
ibrahimuzunoglu <ibrahimuznglu@gmail.com>
2024-11-22 17:41:06 +0300

    update

--- api/server.js
+++ api/server.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-const SECRET_KEY = process.env.SECRET_KEY; 
-const MONGODB_URI = process.env.MONGODB_URI; 
+SECRET_KEY="vatanmilletsakarya";
+MONGODB_URI="mongodb+srv://ibrahim:oZGNRekl3ltKTHxq@aracimipazarla.g7b0j.mongodb.net/aracimipazarla;"
 
 
 app.use(cors());
@@ -27,50 +27,6 @@ mongoose.connect(MONGODB_URI)
     .then(() => console.log('MongoDB’ye başarılı bir şekilde bağlanıldı'))
     .catch(err => console.error('MongoDB bağlantı hatası:', err));
 
-
-const userSchema = new mongoose.Schema({
-    username: String,
-    surname: String,
-    email: { type: String, unique: true },
-    password: String,
-    phonenumber: String,
-    location: String
-});
-
-const User = mongoose.model('User', userSchema);
-
-app.post('/api/register', async (req, res) => {
-    const { username, surname, email, password, phonenumber, location } = req.body;
-    
-    try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'Bu email adresi zaten kayıtlı!' });
-        }
-        
-        const newUser = new User({ username, surname, email, password, phonenumber, location });
-        await newUser.save();
-        res.json({ message: 'Kayıt başarılı!' });
-    } catch (error) {
-        res.status(500).json({ message: 'Kayıt sırasında hata oluştu', error });
-    }
-});
-
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ email, password });
-        if (user) {
-            const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: '12h' }); 
-            res.json({ message: 'Giriş başarılı!', token });
-        } else {
-            res.status(400).json({ message: 'Geçersiz email veya şifre!', success: false });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Giriş sırasında hata oluştu', error });
-    }
-});
 
 
 app.get('/cars', async (req, res) => {
